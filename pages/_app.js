3ff5efc6230aeba48e8f51419dd163fcf25cf1ea
MohamadRasool <msj.developer3133@gmail.com>
2025-01-27 11:23:52 -0800

    update Header

--- pages/_app.js
+++ pages/_app.js
@@ -5,141 +5,85 @@ export default function App({ Component, pageProps }) {
 
   return (
     <div className="container">
-      <div className="sidebar">
-        <input
-          type="checkbox"
-          name="toggle"
-          id="sidebar-toggle"
-          className="sidebar__input"
-        />
-        <label htmlFor="sidebar-toggle" className="sidebar__label">
-          <div className="sidebar__btn"></div>
-        </label>
+    <div class="Sidebar">
+  <input type="checkbox" id="sidebar__input" class="sidebar__input" />
+  <label for="sidebar__input" class="sidebar__label">
+    <div class="Sidebar_menu"></div>
+  </label>
+  <div className="Sidebar_bg"></div>
+  <ul className="Sidebar__list">
+    <li className="Sidebar__item">
+      <a href="index_html" className="item_link">
+      صفحه اصلی
 
-        <div className="sidebar__bg"></div>
-        <ul className="list">
-          <li className="list__item">
-            <a href="index.html" className="list__link">
-              صفحه اصلی
-            </a>
-          </li>
-          <li className="list__item">
-            <a href="#" className="list__link">
-              ویژگی ها
-            </a>
-          </li>
-          <li className="list__item">
-            <a href="" className="list__link">
-              نظرات
-            </a>
-          </li>
-          <li className="list__item">
-            <a href="houses.html" className="list__link">
-              خانه ها
-            </a>
-          </li>
-          <li className="list__item">
-            <a href="#" className="list__link">
-              گالری
-            </a>
-          </li>
-        </ul>
-      </div>
+      </a>
+    </li>
+    <li className="Sidebar__item">
+      <a href="index_html" className="item_link">
+      ویژگی ها
 
-      <header className="header">
-        <img src="img/logo.png" alt="Nexter Logo" className="header__logo" />
-        <h3 className="u-heading-3 u-heading--light">خانه خودتان:</h3>
-        <h1 className="u-heading-1">با خرید خانه نهایت آزادی را احساس کنید</h1>
-        <button className="btn header__btn btn-brown">
-          املاک ما را مشاهده کنید
-        </button>
+      </a>
+    </li>
+    <li className="Sidebar__item">
+      <a href="index_html" className="item_link">
+      نظرات
+      </a>
+    </li>
+    <li className="Sidebar__item">
+      <a href="index_html" className="item_link">
+            خانه
 
+      </a>
+    </li>
+    <li className="Sidebar__item">
+      <a href="index_html" className="item_link">
+      گالری
+      </a>
+    </li>
+  </ul>
+    </div>
+     
+     <Component {...pageProps} />
+     <header className="header">
+      <div className="header__right">
+        <div className="header__right-main">
+        <img src="/img/logo.png" alt="Nexter Logo" className="header__logo" />
+      <h4>خانه خودتان:</h4>
+        <h1>با خرید خانه نهایت آزادی را احساس کنید        </h1>
+        <a className="btn" href="/homes">املاک مارا مشاهده کنید</a>
         <p className="seeon__text">دیده می شود در</p>
-        <figure className="seeon__box-img">
-          <img src="img/logo-bbc.png" alt="BBC" className="seeon__img" />
-          <img src="img/logo-bi.png" alt="BBC" className="seeon__img" />
-          <img src="img/logo-forbes.png" alt="BBC" className="seeon__img" />
-          <img src="img/logo-techcrunch.png" alt="BBC" className="seeon__img" />
+        <figure className="seeon__box">
+          <img src="/img/logo-bbc.png" alt="home" className="seeon__img"/>
+          <img src="/img/logo-bi.png" alt="home" className="seeon__img"/>
+          <img src="/img/logo-forbes.png" alt="home" className="seeon__img"/>
+          <img src="/img/logo-techcrunch.png" alt="home" className="seeon__img"/>
         </figure>
-      </header>
-
-      <div className="real-tors">
-        <p className="real-tors__tittle">سه مالک برتر</p>
-        <div className="real-tors__list">
-          <img
-            src="img/realtor-1.jpeg"
-            alt="real-tors top 1"
-            className="real-tors__img"
-          />
-          <div className="real-tors__details">
-            <h3 className="u-heading-3 u-heading--white">مهدی ایلخانی نسب</h3>
-            <p className="real-tors__text">869 فروش خانه</p>
-          </div>
-
-          <img
-            src="img/realtor-2.jpeg"
-            alt="real-tors top 2"
-            className="real-tors__img"
-          />
-          <div className="real-tors__details">
-            <h3 className="u-heading-3 u-heading--white">کوثر بهشتی</h3>
-            <p className="real-tors__text">243 فروش خانه</p>
-          </div>
-
-          <img
-            src="img/realtor-3.jpeg"
-            alt="real-tors top 3"
-            className="real-tors__img"
-          />
-          <div className="real-tors__details">
-            <h3 className="u-heading-3 u-heading--white">عرشیا احسنی</h3>
-            <p className="real-tors__text">130 فروش خانه</p>
-          </div>
         </div>
       </div>
-
-      <Component {...pageProps} />
-
-      <footer className="footer">
-        <ul className="nav">
-          <li className="nav__item">
-            <a href="#" className="nav__link">
-              خانه رویایی خود را پیدا کنید
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="#" className="nav__link">
-              درخواست پروپوزال
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="#" className="nav__link">
-              برنامه اجاره خانه ها
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="#" className="nav__link">
-              با ما تماس بگیرید
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="#" className="nav__link">
-              ملک خود را ارسال کنید
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="#" className="nav__link">
-              با ما کار کنید
-            </a>
-          </li>
-        </ul>
-
-        <p className="copyright">
-          &copy; حقوق مادی معنوی این سایت برای
-          <strong className="copyright__name">مهدی ایلخانی نسب</strong>
-          محفوظ میباشد
-        </p>
-      </footer>
+     </header>
+     <div className="real-tors">
+  <p className="real-tors__text">سه مالک برتر</p>
+  <div className="real-tors__list">
+    <img src="/img/realtor-1.jpeg" alt="مهدی ایلخانی نسب" />
+    <div className="real-tors__main">
+      <h3>مهدی ایلخانی نسب</h3>
+      <p>869 فروش خانه</p>
     </div>
+    <img src="/img/realtor-2.jpeg" alt="کوثر بهشتی" />
+    <div className="real-tors__main">
+      <h3>کوثر بهشتی</h3>
+      <p>243 فروش خانه</p>
+    </div>
+    <img src="/img/realtor-3.jpeg" alt="عرشیا احسنی" />
+    <div className="real-tors__main">
+      <h3>عرشیا احسنی</h3>
+      <p>130 فروش خانه</p>
+    </div>
+  </div>
+</div>
+
+    </div>
+
+     
   );
 }
