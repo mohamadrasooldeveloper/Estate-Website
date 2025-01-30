2ef918546f110d2bdbbeeceaad592cd5ee68cf39
MohamadRasool <msj.developer3133@gmail.com>
2025-01-30 14:13:23 -0800

    Feature

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,10 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import Feature from "@/components/templates/HomePage/Feature"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
     <div>
-     
+     <Feature/>
     </div>
   );
 }
